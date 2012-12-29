fc3af52ee4e28863ef8c8ed2c722f2b2ee8561e3
Thomas Elsgaard <thomas.elsgaard@gmail.com>
2012-12-30 00:19:19 +0100

    Example sv with REST POST added

--- src/api_resource.erl
+++ src/api_resource.erl
@@ -3,11 +3,30 @@
 %% @doc Example webmachine_resource.
 
 -module(api_resource).
--export([init/1, to_html/2]).
+-compile([export_all]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
-to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+
+allowed_methods(RD, Ctx) ->
+    {['HEAD', 'POST'], RD, Ctx}.
+
+content_types_provided(RD, Ctx) ->
+    {[{"application/json", to_json}], RD, Ctx}.
+
+process_post(RD, Ctx) ->
+    %match the correct sv (subscriber view) from Uri
+    case wrq:path_info(sv, RD) of
+        undefinded ->
+            {true, RD, Ctx};
+        
+        "example" -> 
+            Body = json_body(mochiweb_util:parse_qs(wrq:req_body(RD))),
+            {true, wrq:append_to_response_body(Body, RD), Ctx};
+        _->
+            {true, RD, Ctx}
+    end.
+
+json_body(QS) -> mochijson:encode({struct, QS}).
